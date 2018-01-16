e97c34c37dac442148056d68d2ca12b062e60316
choicedraught <jack.bryant@gmail.com>
2018-01-16 12:56:49 +0800

    Hello world and all that

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const port = 3000; 
+
+let app - express();
+
+
+app.get('/', (req, res) =>{
+	res.send('Hello World');
+});
+
+app.liten(port, () => {
+	console.log('server started on '+port);
+});
